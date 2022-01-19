ddf2d096257367217908e6bcb1db79fefed98fdb
Minin Valentin <63961721+TeanWolf@users.noreply.github.com>
2022-01-19 18:31:48 +0500

    Add files via upload
    Добавлена функция открытия камеры при тряске телефона

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Essentials;
+using IOTProjectt;
 
 namespace IOTProjectt
 {
@@ -18,18 +20,24 @@ namespace IOTProjectt
             Shake.ToggleAccelerometer();
         }
     }
+
+    public static class GlobalVariables
+    {
+        public static bool Shake = false;
+    }
+
     public class DetectShakeTest
     {
         SensorSpeed speed = SensorSpeed.Game;
-        bool Shake = false;
         public DetectShakeTest()
         {
             Accelerometer.ShakeDetected += Accelerometer_ShakeDetected;
+            Accelerometer.ShakeDetected += TakeVideoAsync;
         }
 
         void Accelerometer_ShakeDetected(object sender, EventArgs e)
         {
-            Shake = true;
+            GlobalVariables.Shake = true;
 
         }
 
@@ -51,5 +59,29 @@ namespace IOTProjectt
                 System.Environment.Exit(0);
             }
         }
+
+        public static async void TakeVideoAsync(object sender, EventArgs e)
+        {
+                try
+                {
+                    var video = await MediaPicker.CaptureVideoAsync(new MediaPickerOptions
+                    {
+                        Title = $"project.{DateTime.Now.ToString("dd.MM.yyyy_hh.mm.ss")}.mp4"
+                    });
+
+                    var newFile = Path.Combine(FileSystem.AppDataDirectory, video.FileName);
+                    using (var stream = await video.OpenReadAsync())
+                    using (var newStream = File.OpenWrite(newFile))
+                        await stream.CopyToAsync(newStream);
+                    
+                }
+                catch (Exception ex)
+                {
+                    System.Environment.Exit(0);
+                }
+            }
+        
+
     }
+    
 }
